5008b331ffea3306e92341a60838677f31c8a488
martingalli <martin.galli@wunderman.com>
2018-06-04 18:27:43 -0300

    Adding DOM Controller

--- js/app.js
+++ js/app.js
@@ -9,16 +9,37 @@ let budgetController = (function() {
 // UI CONTROLLER
 let UIController = (function() {
 
+    let DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
 
+    return {
+        getinput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either INC or EXP
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+        getDOMstrings: function() {
+            return DOMstrings
+        }
+    }
 
 })()
 
 // GLOBAL APP CONTROLLER
 let controller = (function(budgetCtrl, UICtrl) {
+
+    let DOM = UICtrl.getDOMstrings()
     
     let ctrlAddItem = function() {
         // 1. Get the field input data
-
+        let input = UICtrl.getinput()
+        console.log(input)
         // 2. Add the item to the budget controller
 
         // Add the item to the UI
@@ -29,7 +50,7 @@ let controller = (function(budgetCtrl, UICtrl) {
 
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
 
     document.addEventListener('keypress', function(event) {
 
